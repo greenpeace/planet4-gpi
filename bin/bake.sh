64530188e82caa371e3693260b1b55a08bfcd3b1
Ray Walker <hello@raywalker.it>
2018-04-13 17:19:33 +1000

    Fix for trap function

--- bin/bake.sh
+++ bin/bake.sh
@@ -4,23 +4,11 @@ set -eu
 function usage() {
   echo "Usage: $(basename "$0") SOURCE_IMAGE [OUTPUT_DIR]
 
-Performs initial composer install in container and exports the resultant site
+Performs initial composer install in container and exports the
+generated files for use elsewhere.
 
 "
 }
-
-source="${BASH_SOURCE[0]}"
-while [[ -h "$source" ]]
-do # resolve $source until the file is no longer a symlink
-  dir="$( cd -P "$( dirname "$source" )" && pwd )"
-  source="$(readlink "$source")"
-  # if $source was a relative symlink, we need to resolve it relative to the
-  # path where the symlink file was located
-  [[ $source != /* ]] && source="$dir/$source"
-done
-BUILD_DIR="$( cd -P "$( dirname "$source" )/.." && pwd )"
-
-# . "${BUILD_DIR}/bin/env.sh"
 
 OPTIONS=':c:e:lprv'
 while getopts $OPTIONS option
@@ -47,13 +35,12 @@ then
 
   # Clean up on exit
   function finish() {
-    rm -fr "$TMPDIR"
+    [[ ! -z "$output_dir" ]] && rm -fr "$output_dir"
   }
-
   trap finish EXIT
-  TMPDIR=$(mktemp -d "${TMPDIR:-/tmp/}$(basename 0).XXXXXXXXXXXX")
 
-  output_dir="$TMPDIR"
+  output_dir=$(mktemp -d "${TMPDIR:-/tmp/}$(basename 0).XXXXXXXXXXXX")
+
 else
   mkdir -p "$2"
   output_dir="$2"
@@ -63,13 +50,10 @@ echo "Image:  $1"
 echo "Output: $output_dir"
 echo ""
 
-# SOURCE_BRANCH_SANITIZED=${SOURCE_BRANCH//[^[:alnum:]_]/-}
-
+# Exit true because bake intentionally aborts container build early
 docker run -e "WP_BAKE=true" "${image}" || true
 
 echo ""
-# docker commit docker "$(docker ps -alq)" gcr.io/planet-4-151612/p4-gpi-app:latest
-# docker tag gcr.io/planet-4-151612/p4-gpi-app:latest "gcr.io/planet-4-151612/p4-gpi-app:${APP_VERSION}"
 
 container="$(docker ps -alq)"
 docker cp "$container:/app/source/public/" "$output_dir"
